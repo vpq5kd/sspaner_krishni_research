d2f092f56cb51a48a9a41c5d33aadeba2a6ed2ad
sophia spaner <vpq5kd@virginia.edu>
2026-01-25 06:12:18 -0500

    added gaussian fit and looked at some basic metrics

--- sspaner_histograms.py
+++ sspaner_histograms.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pandas as pd
 import numpy as np
+import ROOT
 
 pat_data_3 = Path("./pat_data_3")
 
@@ -14,7 +15,36 @@ all_data_df = pd.concat(dfs, ignore_index=True)
 
 organ_name_dict = {"Aorta_KW":"Aorta", "BronchialTree_NoGTV":"BronchialTree", "BronchialTree_Prox":"BronchialTree","BronchialTree_Prox_KW":"BronchialTree", "A_Pulmonary":"PulmonaryArtery", "ChestWal":"CW", "ChestWall":"CW", "ChestWall_L":"CW", "ChestWall_New":"CW", "ChestWall_R":"CW", "Chestwall":"CW","Heart_KW":"Heart","LN-EContour":"LN", "LN-Econtour":"LN", "LN-Econtour-KW":"LN","Lung-ITV":"Lungs", "Lungs-ITV":"Lungs", "Lymph Node":"LN", "Lymph Node-Atlas":"LN", "LymphNode - Econtour":"LN", "LymphNode Atlas":"LN","LymphNode-ATlas":"LN","LymphNode-Atlas":"LN", "LymphNode-Econtour":"LN", "LymphNodeAtlas":"LN","LymphNode_Atlas":"LN","Lymphnode-Atlas":"LN","PA":"PulmonaryArtery","PA_KW":"PulmonaryArtery","PBT":"BronchialTree", "Prox Bronchial Tree":"BronchialTree", "ProximalBronchial_Tree":"BronchialTree","Pulm Art":"PulmonaryArtery","Pulm Artery":"PulmonaryArtery","PulmArt":"PulmonaryArtery","PulmArtery":"PulmonaryArtery","Pulm_Art":"PulmonaryArtery","Pulmonary Artery":"PulmonaryArtery","PulmonaryArteries":"PulmonaryArtery","Pulmonary_A":"PulmonaryArtery","Pulmonary_Artery":"PulmonaryArtery","S_Venacava_V":"SVC","Sup Vena Cava":"SVC","SuperiorVenaCava":"SVC","T Spine":"TSpine","T spine":"TSpine","Thoracic Spine":"TSpine","Thoracic Vertebrae":"TSpine","Thoracic spine":"TSpine","ThoracicSpine":"TSpine","ThoracicSpine dl":"TSpine","ThoracicSpine_KW":"TSpine","Thoracic_Spine":"TSpine","Thoracic_spine":"TSpine","ThoracicSpine":"TSpine","TotalLung-ITV":"Lungs","Tspine":"TSpine","Tspine_vertebral bodies":"TSpine","VC_KW":"VC","V_Cava":"VC","V_CavaSuperior":"SVC","V_Cava_Inf":"IVC","V_Cava_Sup":"SVC","Vena Cava":"VC","VenaCava":"VC","VenaCava Sup":"SVC","VenaCavaSuperior":"SVC","heart":"Heart","pulmonary artery":"PulmonaryArtery","thoracic spine":"TSpine","ThorasicSpine":"TSpine"}
 
+
 all_data_df["Organ_Clean"] = all_data_df["Organ"].replace(organ_name_dict)
 all_data_df = all_data_df[all_data_df["Unnamed: 0"] != 0] #elim control ct for analysis"
-print(all_data_df.head(20))
-    
+all_data_df = all_data_df.rename(columns={"Unnamed: 0":"CT#"})
+
+full_hist = ROOT.TH1F("h",f"Total Fraction Overlap",25,0,1)
+hists = []
+for organ, df_organ in all_data_df.groupby("Organ_Clean"):
+    hist = ROOT.TH1F("h",f"{organ} Fraction Overlap Hist", 25, 0, 1)
+    for val in df_organ["FractionOverlap"]:
+        if val != "NaN":
+            hist.Fill(val)
+            full_hist.Fill(val)
+    fit_result = hist.Fit("gaus", "RS")
+    try: 
+        chi2 = fit_result.Chi2()
+        ndf = fit_result.Ndf()
+        rchi2 = chi2/ndf
+        print(rchi2)
+    except:
+       print("fit not working...") 
+    hists.append(hist)
+full_hist_fit_result = full_hist.Fit("gaus", "RS")
+
+chi2 = full_hist_fit_result.Chi2()
+ndf = full_hist_fit_result.Ndf()
+rchi2 = chi2/ndf
+print(rchi2)
+
+hists.append(full_hist)
+
+
+input("Please press enter to exit...")
