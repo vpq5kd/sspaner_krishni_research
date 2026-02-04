faae9198d36a7b21dc89390847aa047cd65c1291
sophia spaner <vpq5kd@virginia.edu>
2026-02-03 21:44:37 -0500

    believe I fixed the full hist arm splits and updated some stdout messaging appropriately

--- sspaner_histograms_armsplit.py
+++ sspaner_histograms_armsplit.py
@@ -34,6 +34,8 @@ fit_function.SetParNames("Norm", "Lambda")
 fit_function.SetParameters(300,5)
 
 full_hist = ROOT.TH1F("h",f"Total Fraction Overlap",25,0,1)
+full_hist_arm1 = ROOT.TH1F("full hist arm 1",f"Total Fraction Overlap",25,0,1)
+full_hist_arm2 = ROOT.TH1F("full hist arm 2",f"Total Fraction Overlap",25,0,1)
 hists = []
 organs = []
 for (organ,arm), df_organ in all_data_df.groupby(["Organ_Clean","arm"]):
@@ -42,13 +44,19 @@ for (organ,arm), df_organ in all_data_df.groupby(["Organ_Clean","arm"]):
         if val != "NaN":
             hist.Fill(val)
             full_hist.Fill(val)
+
+            if arm == 1:
+                full_hist_arm1.Fill(val)
+            else:
+                full_hist_arm2.Fill(val)
+
     print(f'{organ} arm:{arm} fit results')
     fit_result = hist.Fit(fit_function, "RS")
     try: 
         chi2 = fit_result.Chi2()
         ndf = fit_result.Ndf()
         rchi2 = chi2/ndf
-        hist.SetTitle(f"{organ} Fraction Overlap Hist. Reduced Chi2: {rchi2};Fraction Overlap; Counts")
+        hist.SetTitle(f"{organ} arm:{arm} Fraction Overlap Hist. Reduced Chi2: {rchi2};Fraction Overlap; Counts")
     except:
        print("fit not working for {organ}...")
        continue
@@ -56,6 +64,7 @@ for (organ,arm), df_organ in all_data_df.groupby(["Organ_Clean","arm"]):
     organs.append([organ,arm])
 
 print('all data fit result')
+
 full_hist_fit_result = full_hist.Fit(fit_function, "RS")
 
 chi2 = full_hist_fit_result.Chi2()
@@ -65,6 +74,25 @@ rchi2 = chi2/ndf
 full_hist.SetTitle(f"Fraction Overlap Hist for all Organs. Reduced Chi2: {rchi2};Fraction Overlap; Counts")
 hists.append(full_hist)
 organs.append("Full_Hist")
+
+
+full_hist_arms = [full_hist_arm1, full_hist_arm2]
+arm_full_hist_arms = 1
+for hist in full_hist_arms:
+
+    print(f'Full hist arm: {arm_full_hist_arms} fit result')
+    full_hist_fit_result = hist.Fit(fit_function, "RS")
+
+    chi2 = full_hist_fit_result.Chi2()
+    ndf = full_hist_fit_result.Ndf()
+    rchi2 = chi2/ndf
+
+    hist.SetTitle(f"Fraction Overlap Hist for all Organs in Arm {arm_full_hist_arms}. Reduced Chi2: {rchi2};Fraction Overlap; Counts")
+    hists.append(hist)
+    organs.append("Full_Hist_Arm_{arm_full_hist_arms}")
+    arm_full_hist_arms+=1
+
+
 
 c = ROOT.TCanvas("c","canvas", 800,600)
 organ_index = 0
