245006c5c600ed30d999c4b793eb7a7724444e6f
sophia spaner <vpq5kd@virginia.edu>
2026-02-17 00:12:44 -0500

    found patients with fv = 0

--- data_processing_programs/sspaner_FV_investigation.py
+++ data_processing_programs/sspaner_FV_investigation.py
@@ -5,3 +5,10 @@ selected_patients_df.to_csv("FV_<_0.7.csv")
 
 selected_patients_df["Patient"] = selected_patients_df["Patient"].str[:2]
 print(selected_patients_df["Patient"].unique())
+
+selected_patients_df = df.loc[df["FractionOverlap"] ==0, ["Patient","Organ_Clean","FractionOverlap"]]
+print(selected_patients_df)
+
+selected_patients_df["Patient"] = selected_patients_df["Patient"].str[:2]
+print(selected_patients_df["Patient"].unique())
+
