fa7baaae27609c7db5f180cb7cc54bf0742e78e4
sophia spaner <vpq5kd@virginia.edu>
2026-02-16 23:57:38 -0500

    located patients with a fraction overlap < 0.7

---
+++ data_processing_programs/sspaner_FV_investigation.py
@@ -0,0 +1,4 @@
+from pd3_template import all_data_df as df
+selected_patients_df = df.loc[df["FractionOverlap"] < 0.7, ["Patient","Organ_Clean","FractionOverlap"]]
+print(selected_patients_df)
+selected_patients_df.to_csv("FV_<_0.7.csv")
