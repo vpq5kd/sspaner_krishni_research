e918eecdbe1d1a7043486c6e456dcc0519527515
sophia spaner <vpq5kd@virginia.edu>
2026-02-17 00:47:53 -0500

    made first violin plot

--- data_processing_programs/sspaner_FV_investigation.py
+++ data_processing_programs/sspaner_FV_investigation.py
@@ -1,7 +1,7 @@
 from pd3_template import all_data_df as df
 selected_patients_df = df.loc[df["FractionOverlap"] < 0.7, ["Patient","Organ_Clean","FractionOverlap"]]
 print(selected_patients_df)
-selected_patients_df.to_csv("FV_<_0.7.csv")
+selected_patients_df.to_csv("numeric_outputs/FV_<_0.7.csv")
 
 selected_patients_df["Patient"] = selected_patients_df["Patient"].str[:2]
 print(selected_patients_df["Patient"].unique())
---
+++ data_processing_programs/sspaner_violin_plots.py
@@ -0,0 +1,29 @@
+from pd3_template import all_data_df as df
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df["Organ_Clean"] = (
+    df["Organ_Clean"]
+        .str.strip()
+        .str.lower()
+        .replace({
+            "tspine": "thoracic spine",
+            "pulmonaryartery": "pulmonary artery",
+            "vc": "vena cava",
+            "bronchialtree": "bronchial tree",
+            "spinalcanal": "spinal canal",
+            "cw": "chest wall",
+            "ln": "lymph node"
+        })
+        .str.title()
+)
+
+plt.figure()
+sns.violinplot(data=df, x="Organ_Clean", y="MeanDose",palette="spring")
+plt.xticks(rotation=60)
+plt.title("Mean Dose Distribution for Each Organ".title())
+plt.ylabel("Mean Dose (Gy)")
+plt.xlabel("Organ")
+plt.tight_layout()
+plt.savefig("mean_dose_vp.png")
+plt.show()
