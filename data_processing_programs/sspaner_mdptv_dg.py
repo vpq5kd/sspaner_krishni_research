b793874a58566345146b643ab35c2ea022fe84c5
sophia spaner <vpq5kd@virginia.edu>
2026-04-09 02:14:58 -0400

    updated mdptv dg studyfor paper

--- data_processing_programs/sspaner_mdptv_dg.py
+++ data_processing_programs/sspaner_mdptv_dg.py
@@ -8,17 +8,28 @@ cols = ['DminFromPTV (mm)', 'arm', 'Max_sqrt(grad(Dose)*Dose)_1', 'Organ_Clean',
 df = all_data_df[cols].copy()
 
 
-for (organ, arm), df_ao in df.groupby(['Organ_Clean', 'arm']):
+for organ, df_ao in df.groupby(['Organ_Clean']):
     
-    x = df_ao['DminFromPTV (mm)'].to_numpy()
-    y = df_ao["Max_sqrt(grad(Dose)*Dose)_1"].to_numpy()
-    y = x**2 / df_ao["Max1_Dose"].to_numpy()
+    x1 = df_ao[df_ao["arm"]==1]['DminFromPTV (mm)'].to_numpy()
+    x2 = df_ao[df_ao["arm"]==2]['DminFromPTV (mm)'].to_numpy()
+
+    y1 = df_ao[df_ao["arm"]==1]["Max_sqrt(grad(Dose)*Dose)_1"].to_numpy()
+    y2 = df_ao[df_ao["arm"]==2]["Max_sqrt(grad(Dose)*Dose)_1"].to_numpy()
+    
+    
+    y1 = y1**2 / df_ao[df_ao["arm"]==1]["Max1_Dose"].to_numpy()
+
+    y2 = y2**2 / df_ao[df_ao["arm"]==2]["Max1_Dose"].to_numpy()
+
+    
     plt.figure()
-    plt.scatter(x,y, color='black', marker='+')
+    plt.scatter(x1,y1, color='black', marker='+', label='arm 1')
+    plt.scatter(x2,y2, color='magenta', marker='x',label='arm 2')
+    plt.legend()
     plt.xlabel('Minimum Distance from PTV')
     plt.ylabel('Dose Gradient')
-    plt.title(f'Dose Gradient vs. Minimum Distance from PTV for {organ} in arm {arm}')
-    filename = f'mdptv_dg_study/{organ}_{arm}.png'
+    plt.title(f'Dose Gradient vs. Minimum Distance from PTV for {organ[0]}')
+    filename = f'mdptv_dg_study/{organ[0]}.png'
     plt.savefig(filename)
     print(f'Saved {filename}.')
     plt.close() 
